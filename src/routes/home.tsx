66729a491e8a08e5599297726c6bc9f85df8667b
MR-Abdur-Rahman <abdhu101805@gmail.com>
2026-06-20 22:11:32 +0530

    remove that above X mark from notification sheet of home page

--- src/routes/home.tsx
+++ src/routes/home.tsx
@@ -878,10 +878,6 @@ function NotificationSheet({ open, onOpenChange, notifications, onNavigate }: {
               </DropdownMenuContent>
             </DropdownMenu>
           </div>
-          <button type="button" onClick={() => onOpenChange(false)}
-            className="h-8 w-8 flex items-center justify-center rounded-full bg-secondary">
-            <X className="h-4 w-4" />
-          </button>
         </div>
         <div className="flex-1 overflow-y-auto divide-y divide-border">
           {visible.length === 0 ? (
